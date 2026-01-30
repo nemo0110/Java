f0513ff4791bdb87f459b9857d67fec205cce92f
nemo0110 <66428042+nemo0110@users.noreply.github.com>
2026-01-30 11:27:56 +0100

    Create MapToArrayOrder.java

---
+++ Map/MapToArrayOrder.java
@@ -0,0 +1,8 @@
+//Class made for mapToArrayAdvanced6()
+public enum Order {
+    INSERTION,  // mantieni ordine originale (usa LinkedHashMap)
+    KEY_ASC,    // ordina per chiave crescente
+    KEY_DESC,   // ordina per chiave decrescente
+    VALUE_ASC,  // ordina per valore crescente
+    VALUE_DESC  // ordina per valore decrescente
+}
